46e8903ec47c609235e1c21d18ec278f4e4ae05e
mehedy201 <mehedy643@gmail.com>
2026-05-04 14:08:22 +0600

    remove unused code

--- firebase.config.js
+++ firebase.config.js
@@ -1,32 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCH_YwYgSecN1baSujOHwlH-0D2UPI3mUY",
-//   authDomain: "event-tickets-manage-site.firebaseapp.com",
-//   projectId: "event-tickets-manage-site",
-//   storageBucket: "event-tickets-manage-site.firebasestorage.app",
-//   messagingSenderId: "1049016446011",
-//   appId: "1:1049016446011:web:b7668b0157b1619104001e",
-//   measurementId: "G-56BPKEX97X"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-
-
-
-
-
-
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics";
